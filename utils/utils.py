32d51715d3d9af4b0653ce5ee97a53d6613c2b26
panzaresce <panarellimarco99@gmail.com>
2025-03-26 20:23:58 +0100

    File clean

--- utils/utils.py
+++ utils/utils.py
@@ -10,98 +10,9 @@ from utils.config import *
 def extract_model_name(card):
     return card.split("/")[-1]
 
-def get_bnb_config():
-    return transformers.BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
-
 def load_dataset(path, split="test"):
     ds = load_from_disk(path)
     return ds[split]
-
-def load_model_and_tokenizer(model_card, bnb_config):
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_card,
-        device_map="auto",
-        quantization_config=bnb_config,
-        trust_remote_code=True,
-    )
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_card)
-    return model, tokenizer
-
-def create_pipeline(model, tokenizer):
-    return transformers.pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        pad_token_id=tokenizer.eos_token_id,
-    )
-
-def generate_responses(pipe, ds_test, base_prompt):
-    responses = []
-    for clause, label in tqdm(zip(ds_test["text"], ds_test["labels"]), total=len(ds_test)):
-        messages = [{"role": "system", "content": base_prompt},
-                    {"role": "user", "content": clause}]
-        gen_out = pipe(messages, max_new_tokens=50, temperature=0, do_sample=False)[0]
-        gen_out["labels"] = label
-        responses.append(gen_out)
-    return responses
-
-def compute_f1_score(responses, label_to_id):
-    y_true, y_pred = [], []
-    for r in responses:
-        resp_tags = [t for t in r["generated_text"][2]["content"].replace("<", "").replace(">", "").replace(",", "").split(" ")]
-
-        true_sample = [0] * len(label_to_id)
-        for t in r["labels"]:
-            true_sample[t] = 1
-
-        pred_sample = [0] * len(label_to_id)
-        if len(resp_tags) <= len(label_to_id):
-            for t in resp_tags:
-                try:
-                    pred_sample[label_to_id[t]] = 1
-                except KeyError as e:
-                    print(f"WARNING: {e}, initializing sample as list of zeros")
-
-        y_true.append(true_sample)
-        y_pred.append(pred_sample)
-
-    macro_f1 = f1_score(y_true, y_pred, average='macro', zero_division=0)
-    micro_f1 = f1_score(y_true, y_pred, average='micro', zero_division=0)
-    return {"macro": macro_f1, "micro": micro_f1}
-
-def evaluate_models(endpoints, ds_test, ds_val, base_prompt):
-    label_to_id = LABEL_TO_ID
-    bnb_config = get_bnb_config()
-    models_score = {}
-
-    for name, model_card in endpoints.items():
-        print(f"------------------------ Prompting {name} ------------------------")
-
-        model, tokenizer = load_model_and_tokenizer(model_card, bnb_config)
-        pipe = create_pipeline(model, tokenizer)
-
-        # Evaluate on test dataset
-        test_responses = generate_responses(pipe, ds_test, base_prompt)
-        test_scores = compute_f1_score(test_responses, label_to_id)
-
-        if ds_val != None:
-            # Evaluate on validation dataset
-            validation_responses = generate_responses(pipe, ds_val, base_prompt)
-            validation_scores = compute_f1_score(validation_responses, label_to_id)
-        else:
-            validation_scores = {"macro": 0.0, "micro": 0.0}
-
-        models_score[name] = {
-            "test": test_scores,
-            "validation": validation_scores
-        }
-
-    return models_score
 
 
 def show_confusion_matrix(confusion_matrix, id2label):
